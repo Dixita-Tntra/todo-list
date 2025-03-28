b28aa30073bf6a233811912f4c755975c1ea40fd
Dixita-Tntra <dixita.kataria@tntra.io>
2025-03-28 16:49:10 +0530

    Components, css & readmeupdated

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+
+function TodoList() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleInputChange = (e) => setNewTask(e.target.value);
+  function addTask() {
+    if (newTask.trim() !== "") {
+      setTasks((t) => [...t, { text: newTask, completed: false }]);
+      setNewTask("");
+    }
+  }
+  function deleteTask(index) {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  }
+  function moveTaskUp(index) {
+    if (index > 0) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index - 1]] = [
+        updatedTasks[index - 1],
+        updatedTasks[index],
+      ];
+      setTasks(updatedTasks);
+    }
+  }
+  function moveTaskDown(index) {
+    if (index < tasks.length - 1) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index + 1]] = [
+        updatedTasks[index + 1],
+        updatedTasks[index],
+      ];
+      setTasks(updatedTasks);
+    }
+  }
+  function completeTask(index) {
+    setTasks(
+      tasks.map((task, i) =>
+        i === index ? { ...task, completed: !task.completed } : task
+      )
+    );
+  }
+
+  return (
+    <div className="todo-list">
+      <h1>To-Do List</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter a task..."
+          value={newTask}
+          onChange={handleInputChange}
+        />
+        <button className="add-button" onClick={addTask}>
+          Add
+        </button>
+      </div>
+      <ol>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <input
+              type="checkbox"
+              checked={task.completed}
+              onChange={() => completeTask(index)}
+            />
+            <span className="text">
+              {task.completed ? <del>{task.text}</del> : task.text}
+            </span>
+
+            <button className="delete-button" onClick={() => deleteTask(index)}>
+              Delete
+            </button>
+            <button className="move-button" onClick={() => moveTaskUp(index)}>
+              ⬆️
+            </button>
+            <button className="move-button" onClick={() => moveTaskDown(index)}>
+              ⬇️
+            </button>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+}
+
+export default TodoList;
